2e7b753487ce1e1b5b79f10495f43a360cbc6b91
tiencuong07 <20020375@vnu.edu.vn>
2023-05-07 22:58:39 +0700

    fix đường dẫn

--- client/src/displays/admin/admin.jsx
+++ client/src/displays/admin/admin.jsx
@@ -8,12 +8,14 @@ import {
     Navigate
   } from "react-router-dom";
 import BorrowingOff from './pages/borrowingOff/borrowingOff';
+import Statistical from './pages/statistical/statistical'
 
 export default function Admin() {
     return(
         <div className="container">
             <Router>
                 <Routes>
+                    <Route path='/' element = {<Statistical/>}/>
                     <Route path='/infoBorrowsOff' element = {<BorrowingOff/>}/>
                 </Routes>
             </Router>
